629890ab39712a21b46bce448212078799fa6a65
Karol Balejko <info@groovydev.org>
2011-12-05 13:08:20 +0100

    proxool dependency update

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,6 +22,8 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        
+        mavenRepo "http://repo.fusesource.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
